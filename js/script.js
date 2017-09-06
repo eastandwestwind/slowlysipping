ec000d139e6c966c5d5822994066d2cfd29c99c3
Catherine Smith <eastandwestwind@gmail.com>
2017-09-06 10:57:43 -0400

    adding site search

--- js/script.js
+++ js/script.js
@@ -23,4 +23,18 @@ $(document).ready(function() {
     $('.off-canvas-container').toggleClass('is-active');
   });
 
+  // Shows the number of search results.
+  jQuery('#search-input').on('focus', function() {
+    jQuery('.show-results-count').addClass('active');
+    jQuery('.results-container').show();
+  });
+
+  // Hides the number of search results.
+  jQuery(document).on('click focus', function(e) {
+    if (jQuery(e.target).closest('#search-input').length === 0) {
+      jQuery('.show-results-count').removeClass('active');
+      jQuery('.results-container').hide();
+    }
+  });
+
 });
